1230a3d6881cf0f5295b87bc98012bb25dbc897f
飞飞 <zhengsujie@zuzuche.com>
2020-08-05 17:45:25 +0800

    打包一个文件

--- webpackConfig/webpack.config.js
+++ webpackConfig/webpack.config.js
@@ -1,8 +1,14 @@
+const path = require('path') ;
 module.exports = {
-  mode: "development",
-  //相对于process.cwd(),process.cwd()返回的是Node.js进程的当前工作目录，此处是workplace。
-  entry: "./src/index.js",
-  output: {
-    filename: "js/[name].[hash:8].js",
-  },
+  // 打包模式
+  mode:'development',
+  // 相对路径相对GuFeiFei
+  // webpack是基于node 此处其实是相对于process.cwd(),process.cwd()返回的是Node.js进程的当前工作目录，此处是GuFeiFei。
+  entry:'./src/index.js',
+  output:{
+    // 不设置path则打包文件放在GuFeiFei/dist内
+    // 设置path则打包文件放在设置的文件夹内
+    filename:'./[name].js',
+    path:path.resolve(__dirname,'../dist')
+  }
 }
