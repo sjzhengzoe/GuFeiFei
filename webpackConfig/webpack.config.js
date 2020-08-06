ef9b7e5827cca5800eb9adf73add07d300f64453
飞飞 <zhengsujie@zuzuche.com>
2020-08-06 17:02:21 +0800

    打包css格式文件 以<style>的方式插入

--- webpackConfig/webpack.config.js
+++ webpackConfig/webpack.config.js
@@ -1,14 +1,19 @@
-const path = require('path') ;
+const path = require("path")
 module.exports = {
-  // 打包模式
-  mode:'development',
-  // 相对路径相对GuFeiFei
-  // webpack是基于node 此处其实是相对于process.cwd(),process.cwd()返回的是Node.js进程的当前工作目录，此处是GuFeiFei。
-  entry:'./src/index.js',
-  output:{
-    // 不设置path则打包文件放在GuFeiFei/dist内
-    // 设置path则打包文件放在设置的文件夹内
-    filename:'./[name].js',
-    path:path.resolve(__dirname,'../dist')
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "./[name].js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      }
+    ]
   }
 }
